15016ad01d80d60c5bc21fdbd35a9585ae56b206
Jeremy Wolff <jwolff@dsdmail.net>
2018-04-19 14:50:56 -0600

    Added Music and changed level from splash to level 1.

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("StartLevel", 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartLevel() {
+        SceneManager.LoadScene(1);
+    }
+}
